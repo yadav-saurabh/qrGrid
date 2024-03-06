48962233bf0da57e6426d65133db3c83149c41ce
yadav-saurabh <yadav-saurabh@outlook.com>
2024-03-06 08:22:45 +0530

    refactor: finderPattern and alignmentPattern

--- index.ts
+++ index.ts
@@ -1,59 +1,63 @@
-const FINDER_PATTERN_SIZE = {
-  outer: 7,
-  inner: 3,
-};
-const QR_SIZE = 21;
+const FINDER_PATTERN_SIZE = 7;
 
-const dataArray = new Uint8Array(QR_SIZE * QR_SIZE);
+class QR {
+  data: Uint8Array;
+  size: number;
 
-for (let i = 0; i < FINDER_PATTERN_SIZE.outer; i++) {
-  for (let j = 0; j < FINDER_PATTERN_SIZE.outer; j++) {
-    const topRightIndex = i * QR_SIZE + j;
-    const topLeftIndex = i * QR_SIZE + QR_SIZE - j - 1;
-    const bottomRightIndex = (QR_SIZE - i - 1) * QR_SIZE + j;
+  constructor(size: number) {
+    this.size = size;
+    this.data = new Uint8Array(size * size);
+    this.fillBlock(FINDER_PATTERN_SIZE);
+    this.print();
+  }
 
-    if (j === 0 || j === FINDER_PATTERN_SIZE.outer - 1) {
-      dataArray[topRightIndex] = 1;
-      dataArray[topLeftIndex] = 1;
-      dataArray[bottomRightIndex] = 1;
+  // fill block
+  // finderPattern -> patternSize : 7
+  // timelineBlock -> patternSize : 5
+  fillBlock(patternSize: number, x = 0, y = 0) {
+    const height = patternSize + x - 1;
+    const width = patternSize + y - 1;
+    for (let i = x; i <= height; i++) {
+      for (let j = y; j <= width; j++) {
+        const topLIndex = i * this.size + j;
+        const topRIndex = i * this.size + this.size - j - 1;
+        const bottomLIndex = (this.size - i - 1) * this.size + j;
+
+        // outer block
+        if (j === y || j === width) {
+          this.data[topLIndex] = 1;
+          this.data[topRIndex] = 1;
+          this.data[bottomLIndex] = 1;
+        }
+        if (i === x || i === height) {
+          this.data[topLIndex] = 1;
+          this.data[topRIndex] = 1;
+          this.data[bottomLIndex] = 1;
+        }
+        // inner block
+        if (j >= y + 2 && j <= width - 2 && i >= x + 2 && i <= height - 2) {
+          this.data[topLIndex] = 1;
+          this.data[topRIndex] = 1;
+          this.data[bottomLIndex] = 1;
+        }
+      }
     }
-    if (i === 0 || i === FINDER_PATTERN_SIZE.outer - 1) {
-      dataArray[topRightIndex] = 1;
-      dataArray[topLeftIndex] = 1;
-      dataArray[bottomRightIndex] = 1;
-    }
-    if (
-      j !== 1 &&
-      j !== FINDER_PATTERN_SIZE.outer - 2 &&
-      (i === FINDER_PATTERN_SIZE.inner - 1 ||
-        i === FINDER_PATTERN_SIZE.inner + 1)
-    ) {
-      dataArray[topRightIndex] = 1;
-      dataArray[topLeftIndex] = 1;
-      dataArray[bottomRightIndex] = 1;
-    }
-    if (
-      i !== 1 &&
-      i !== FINDER_PATTERN_SIZE.outer - 2 &&
-      (j === FINDER_PATTERN_SIZE.inner - 1 ||
-        j === FINDER_PATTERN_SIZE.inner + 1)
-    ) {
-      dataArray[topRightIndex] = 1;
-      dataArray[topLeftIndex] = 1;
-      dataArray[bottomRightIndex] = 1;
+  }
+
+  print() {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        const index = i * this.size + j;
+        if (this.data[index] === 1) {
+          process.stdout.write("██");
+          // process.stdout.write("1 ");
+          continue;
+        }
+        process.stdout.write("0 ");
+      }
+      process.stdout.write("\n");
     }
   }
 }
 
-for (let i = 0; i < QR_SIZE; i++) {
-  for (let j = 0; j < QR_SIZE; j++) {
-    const index = i * QR_SIZE + j;
-    if (dataArray[index] === 1) {
-      process.stdout.write("██");
-      // process.stdout.write("1 ");
-      continue;
-    }
-    process.stdout.write("0 ");
-  }
-  process.stdout.write("\n");
-}
+new QR(21);
