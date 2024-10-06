6356bb5d37746df33c1b378f2887d76277ca4517
yadav-saurabh <yadav-saurabh@outlook.com>
2024-10-06 12:42:49 +0530

    feat(angular): submodule entry point

--- packages/angular/src/public-api.ts
+++ packages/angular/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of angular
  */
 
-export { Qr as CanvasQr } from "./canvas";
-export { Qr as SvgQr } from "./svg";
+export { Qr as CanvasQr } from "./canvas/public-api";
+export { Qr as SvgQr } from "./svg/public-api";
